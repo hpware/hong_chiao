fb07893b3d718c74cf8823b059454764d6d6d09b
Howard <hw@yuanhau.com>
2026-08-12 01:16:44 +0800

    fix title thing

---
+++ app/leave/layout.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from "next";
+import type { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "假單管理",
+};
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return children;
+}
