9643deb4e40a14d7dcfe4996fa112b63f46d4ae4
Howard <hw@yuanhau.com>
2026-09-13 00:18:09 +0800

    Remove the `leave` component being accessed.

--- app/leave/layout.tsx
+++ app/leave/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { notFound } from "next/navigation";
 import type { ReactNode } from "react";
 
 export const metadata: Metadata = {
@@ -6,5 +7,6 @@ export const metadata: Metadata = {
 };
 
 export default function Layout({ children }: { children: ReactNode }) {
+  notFound();
   return children;
 }
